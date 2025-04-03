34beb3b72c584cc4ca3cd68815baa32d6e0505ad
Essu-man <essumanconstance514@gmail.com>
2025-04-03 11:30:42 +0000

    layout

--- mngsystem/src/App.tsx
+++ mngsystem/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import EggLogging from './pages/EggLogging';
 import FeedManagement from './pages/FeedManagement';
 import MedicationTracking from './pages/MedicationTracking';
 
-const App: React.FC = () => {
+function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-white">
-        <div className="pt-4">
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/egg-logging" element={<EggLogging />} />
-            <Route path="/feed-management" element={<FeedManagement />} />
-            <Route path="/medication-tracking" element={<MedicationTracking />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        {/* Redirect root path to Dashboard */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+        {/* Other routes */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/egg-logging" element={<EggLogging />} />
+        <Route path="/feed-management" element={<FeedManagement />} />
+        <Route path="/medication-tracking" element={<MedicationTracking />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
